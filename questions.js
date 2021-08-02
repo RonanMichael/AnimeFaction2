0584d59ee1b9dcb0251aac8feb5295df238a6b95
RonanMichael <44853197+RonanMichael@users.noreply.github.com>
2021-08-02 22:49:33 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "What is the name of Todoroki's quirk?",
+    choice1: "Half-Cold Half-Hot",
+    choice2: "Half-Hot Half-Cold",
+    choice3: "Half-Ice Half-Fire",
+    choice4: "Half and Half",
+    answer: 0
+},
+            {
+            question: "How many siblings does Todoroki have?",
+    choice1: "2",
+    choice2: "3",
+    choice3: "4",
+    choice4: "1",
+    answer: 1
+},
+            {
+            question: "How did Todoroki get his scar?",
+    choice1: "An accident with his mom.",
+    choice2: "A fight against a villain.",
+    choice3: "no reason",
+    choice4: "powers gone wrong",
+    answer: 0
+},
+            {
+            question: "Why didn't Todoroki like using his fire powers?",
+    choice1: "dad issues",
+    choice2: "was scared of fire",
+    choice3: "hated sand",
+    choice4: "too coool for that",
+    answer: 0
+},
+            {
+            question: "How did Todoroki get ito U.A. High School?",
+    choice1: "Reccommendation",
+    choice2: "grades",
+    choice3: "passed a test",
+    choice4: "lotto",
+    answer: 0
+},
+            {
+            question: "What covered half of Todoroki's first hero costume?",
+    choice1: "fire",
+    choice2: "ice",
+    choice3: "gravel",
+    choice4: "smiley faces",
+    answer: 1
+},
+            {
+               question: "How did Todoroki rank among those recommended to U.A. that year??",
+    choice1: "1st",
+    choice2: "3rd",
+    choice3: "2nd",
+    choice4: "4th",
+    answer: 2
+},{
+               question: "Who finally convinced Todoroki to use his fire powers?",
+    choice1: "himself",
+    choice2: "Deku",
+    choice3: "Momo Yaoyorozu",
+    choice4: "Katsuki Bakugo",
+    answer: 1
+},
+            {
+               question: "Who did Todoroki do his hero internship with?",
+    choice1: "All Might",
+    choice2: "His DAd",
+    choice3: "hawks",
+    choice4: "Gran Torino",
+    answer: 1
+},
+            {
+               question: "Who did Todoroki team up with for his final exams?",
+    choice1: "Minoru Mineta",
+    choice2: "Izuku Midoriya",
+    choice3: "Fumikage Tokoyami",
+    choice4: "Momo Yaoyorozu",
+    answer: 3
+}];
